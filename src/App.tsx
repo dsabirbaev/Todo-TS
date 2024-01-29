0db6af399b4a6da759e69e6b1ce8101f5916e72d
dsabirbaev <davran9559@gmail.com>
2024-01-29 15:48:22 +0500

    add interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-
+import { useState } from "react";
 
 import styles from "./home.module.css";
-
+import { IData } from "./interfaces";
 const App = (): JSX.Element => {
+
+  const data: IData[] = [
+    {title: "Name1", id: 1, description: "Description1"},
+    {title: "Name2", id: 2, description: "Description2"},
+    {title: "Name3", id: 3, description: "Description3"}
+  ];
+
+  const [title, setTitle] = useState <string>();
+  
+  const [arr, setArr] = useState <IData[]>(data);
+  
+
+  const handleSubmit = (): void => {
+    if(!title?.length) return;
+
+    setTitle('');
+  }
+
   return (
     <div className={styles.todo}>
       <h1 className={styles.title}>App Todo</h1>
-      
-      <input type="text" placeholder="Enter to" className={styles.input}/>
-      <button className={styles.button}>Add Todo</button>
+
+      <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Enter to" className={styles.input}/>
+      <button onClick={handleSubmit} className={styles.button}>Add Todo</button>
       
     </div>
   )
---
+++ src/interfaces/index.ts
@@ -0,0 +1,7 @@
+
+
+export interface IData {
+    title: string;
+    id: number;
+    description: string;
+}
